40fd75ba68a40980178004a0aee4b2cd86c076f7
Matthew Davis <2matthewd1@gmail.com>
2026-03-03 15:01:48 -0500

    Created input generator, 2 example files, and started main file with basic file reading/input

---
+++ cache_simulator.py
@@ -0,0 +1,9 @@
+
+
+
+if __name__ == '__main__':
+    with open("inputs/example_input1.txt") as file:
+        first_line = file.readline().strip()
+        print(first_line)
+        second_line = file.readline().strip()
+        print(second_line)

7cd2d57f48b8fa7c034181c613330d423028548b
willzoo <zhujwill@gmail.com>
2026-03-05 21:46:45 -0500

    extract requests and cache size from read input

--- cache_simulator.py
+++ cache_simulator.py
@@ -1,9 +1,23 @@
+import sys
 
+DEFAULT_INPUT_FILE = "inputs/example_input1.txt"
 
 
 if __name__ == '__main__':
-    with open("inputs/example_input1.txt") as file:
+    # input files are stored in the inputs folder, pass the path as a argument
+    input_file = sys.argv[1] if len(sys.argv) > 1 else DEFAULT_INPUT_FILE
+
+    with open(input_file) as file:
         first_line = file.readline().strip()
-        print(first_line)
         second_line = file.readline().strip()
-        print(second_line)
+
+    k = int(first_line.split(" ")[0])
+    print("[INFO]: Cache size: ", k)
+    
+    requests = []
+    for i, request in enumerate(second_line.split(" ")):
+        request = int(request)
+        requests.append(request)
+    print("[INFO]: Requests: ", requests)
+
+    fifo_misses = fifo(k, requests.copy())

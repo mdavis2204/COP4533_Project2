d1f671f2f3d8d1bcf4d21b7c43a14a58421a939a
Matthew Davis <2matthewd1@gmail.com>
2026-03-06 19:17:22 -0500

    Fixed request generator to not have 0 misses, added request multiplier

--- request_generator.py
+++ request_generator.py
@@ -2,13 +2,16 @@
 # Prints out cache size, number of requests, then a new line of
 
 # Cache Size is the size of the cache excluding 0, 0 1 2 3 would be 4
-cache_size = 4
+cache_size = 16
 
 # Request num is the number of requests excluding 0
-request_num = 32
+request_num = 50
+
+# Request multiplier times cache_size is the max number of requests
+request_multiplier = 4
 
 print(cache_size, request_num)
 
 import random
 for _ in range(request_num):
-    print(random.randrange(0, cache_size), end=" ")
+    print(random.randrange(0, cache_size * request_multiplier), end=" ")
